c7a250ddd1aae185901381f05286be26ece289fa
Ivan Ruiz Manuel <ivan.ruiz@unige.ch>
2023-01-25 12:25:37 +0100

    v1.0 finished! Missing: better storage, y0 matching, calibration separate from data...

--- analysis/restore_plots.py
+++ analysis/restore_plots.py
@@ -11,6 +11,11 @@
 import networkx as nx
 import pandas as pd
 
+from gen_utils.cnf_tools import ConfigHandler
+from gen_utils import fig_tools
+
+fig_tools.plt.rcParams['axes.prop_cycle'] = fig_tools.plt.cycler(color=fig_tools.plt.cm.tab20c.colors)
+
 
 def plot_io_network(*in_out: pd.DataFrame, labels=True):
     """Create a network graph using input/output dataframes.
@@ -33,3 +38,82 @@ def plot_io_network(*in_out: pd.DataFrame, labels=True):
     adjacency_df = adjacency_df.notnull().astype(int)
     network = nx.from_pandas_adjacency(adjacency_df)
     nx.draw_networkx(network, node_size=100, font_size=6, with_labels=labels)
+
+
+def plot_fout(model, handler: ConfigHandler, flow):
+    """Plot values flowing out of elements at a flow node."""
+    columns = [e for f, e in model.FoE if f == flow]
+    fout_df = pd.DataFrame(index=model.Years, columns=columns)
+    for f, e in model.FoE:
+        if f == flow:
+            for y in model.Years:
+                fout_df.loc[y, e] = model.TPERIOD * sum(model.fout[f, e, y, h].value for h in model.Hours)
+
+    axis = fout_df.plot.area(linewidth=0)
+
+    hist_values = [handler.get_flow_value(flow, "actual_flow", y) for y in model.Years]
+    actual = pd.Series(data=hist_values, index=model.Years, name="Historical total")
+    axis = actual.plot.line(ax=axis, color="black", linestyle="-.")
+    axis.set_title(f"FoE at {flow}")
+    handles, labels = fig_tools.get_plt_inverted_legend(axis)
+    axis.legend(handles, labels, bbox_to_anchor=(1.1, 1.05))
+    fig_tools.plt.tight_layout()
+    axis.autoscale()
+
+    return axis
+
+
+def plot_fin(model, handler: ConfigHandler, flow):
+    """Plot values flowing into elements at a flow node."""
+    columns = [e for f, e in model.FiE if f == flow]
+    fin_df = pd.DataFrame(index=model.Years, columns=columns)
+    for f, e in model.FiE:
+        if f == flow:
+            for y in model.Years:
+                fin_df.loc[y, e] = model.TPERIOD * sum(model.fin[f, e, y, h].value for h in model.Hours)
+
+    axis = fin_df.plot.area(linewidth=0)
+
+    hist_values = [handler.get_flow_value(flow, "actual_flow", y) for y in model.Years]
+    actual = pd.Series(data=hist_values, index=model.Years, name="Historical total")
+    axis = actual.plot.line(ax=axis, color="black", linestyle="-.")
+    axis.set_title(f"FiE at {flow}")
+    handles, labels = fig_tools.get_plt_inverted_legend(axis)
+    axis.legend(handles, labels, bbox_to_anchor=(1.1, 1.05))
+    fig_tools.plt.tight_layout()
+    axis.autoscale()
+
+    return axis
+
+
+def plot_process_act(model, handler: ConfigHandler, process, trd_dir=None, axis=None, title: bool = False):
+    """Plot activity values at a process element."""
+    activity = pd.Series(index=model.Years, name=process)
+    if process in model.Trades:
+        if trd_dir == "imp":
+            act = model.aimp
+            value_type = "actual_import"
+        elif trd_dir == "exp":
+            act = model.aexp
+            value_type = "actual_export"
+        else:
+            raise ValueError("'trd_dir' must be specified as 'imp' or 'exp' for", process)
+    else:
+        act = model.a
+        value_type = "actual_activity"
+
+    for y in model.Years:
+        activity[y] = model.TPERIOD * sum(act[process, y, h].value for h in model.Hours)
+    axis = activity.plot.line(ax=axis)
+
+    hist_values = [handler.get_process_value(process, value_type, y) for y in model.Years]
+    actual = pd.Series(data=hist_values, index=model.Years, name="Historical total")
+    axis = actual.plot.line(ax=axis, color="black", linestyle="-.")
+    if title is not None:
+        axis.set_title(process)
+    handles, labels = fig_tools.get_plt_inverted_legend(axis)
+    axis.legend(handles, labels, bbox_to_anchor=(1.1, 1.05))
+    fig_tools.plt.tight_layout()
+    axis.autoscale()
+
+    return axis
